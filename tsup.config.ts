56ca7735020e279b9b222384baf3226a74283ff9
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2024-12-16 20:42:00 +0200

    fix(package): Add "use client" banner to client dist file

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,22 +1,37 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  clean: true,
-  minify: true,
-  entry: {
-    cli: "src/cli/index.ts",
-    middleware: "src/middleware/index.ts",
-    "index.client.template": "src/router/client/index.ts",
-    "index.client": "src/router/client/index.ts",
-    "index.server.template": "src/router/server/index.ts",
-    "index.server": "src/router/server/index.ts",
-  },
-  dts: {
+export default defineConfig([
+  {
+    clean: true,
+    minify: true,
     entry: {
       cli: "src/cli/index.ts",
       middleware: "src/middleware/index.ts",
+      "index.server.template": "src/router/server/index.ts",
+      "index.server": "src/router/server/index.ts",
+    },
+    dts: {
+      entry: {
+        cli: "src/cli/index.ts",
+        middleware: "src/middleware/index.ts",
+      },
+    },
+  },
+  {
+    clean: true,
+    minify: true,
+    esbuildOptions(options) {
+      options.banner = { js: '"use client"' };
+    },
+    entry: {
       "index.client.template": "src/router/client/index.ts",
       "index.client": "src/router/client/index.ts",
     },
+    dts: {
+      entry: {
+        "index.client.template": "src/router/client/index.ts",
+        "index.client": "src/router/client/index.ts",
+      },
+    },
   },
-});
+]);
