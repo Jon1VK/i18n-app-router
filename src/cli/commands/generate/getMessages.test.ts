e9b3390e544f950d8a51408eb4f9528273b61b1f
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2024-12-16 11:49:49 +0200

    test(package): Add test for getMessages function

---
+++ src/cli/commands/generate/getMessages.test.ts
@@ -0,0 +1,32 @@
+import path from "path";
+import { describe, expect, test } from "vitest";
+import { DEFAULT_CONFIG } from ".";
+import { getMessages } from "./getMessages";
+
+const exampleDir = path.join(process.cwd(), "./src/__mocks__/messages");
+
+describe("getMessages()", () => {
+  test("works correctly", async () => {
+    const messages = await getMessages({
+      ...DEFAULT_CONFIG,
+      locales: ["fi", "en"],
+      getMessages: async (locale) => {
+        return (await import(`${exampleDir}/${locale}.json`)).default;
+      },
+    });
+    expect(messages).toStrictEqual({
+      fi: {
+        "hello.world": "Hei maailma",
+        "hello.name": "Hei {name}",
+        projects:
+          "{count, plural, =0 {Ei projekteja} one {Yksi projekti} other {# projektia}",
+      },
+      en: {
+        "hello.world": "Hello world",
+        "hello.name": "Hello {name}",
+        projects:
+          "{count, plural, =0 {No project} one {One project} other {# projects}",
+      },
+    });
+  });
+});
